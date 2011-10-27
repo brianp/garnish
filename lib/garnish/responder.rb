96e9f01e530da1e5076221d0e5aa94f350a3c4e7
brianp <brian.o.pearce@gmail.com>
2011-10-26 21:09:19 -0700

    Will now work with arrays of records.

--- lib/garnish/responder.rb
+++ lib/garnish/responder.rb
@@ -4,14 +4,25 @@ module Garnish
       vars = controller.instance_variables.reject {|x| x.to_s.include?("@_")}
 
       vars.each do |var|
-        record = controller.instance_variable_get((var.respond_to?(:each) ? var.first : var).to_s)
-        klass = record.class
+        record = controller.instance_variable_get(var.to_s)
+
+        if record.respond_to?(:each)
+          klass = record.first.class
+        else
+          klass = record.class
+        end
+
         presenter_name = "#{klass.to_s}Presenter"
 
         if class_exists?(presenter_name.to_sym)
-          presenter = presenter_name.constantize.new(record, controller.view_context)
-          controller.instance_variable_set(var.to_s, presenter)
+          if record.respond_to?(:each)
+            presenter = record.map{ |v| presenter_name.constantize.new(v, controller.view_context) }
+          else
+            presenter = presenter_name.constantize.new(record, controller.view_context)
+          end
         end
+
+        controller.instance_variable_set(var.to_s, presenter)
       end
 
       super

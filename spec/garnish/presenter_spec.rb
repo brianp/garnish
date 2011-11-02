b7cf5d97090d27b0bb88abac7538bd4d64df0213
brianp <brian.o.pearce@gmail.com>
2011-11-02 01:54:49 -0700

    Added field tests.

--- spec/garnish/presenter_spec.rb
+++ spec/garnish/presenter_spec.rb
@@ -5,6 +5,28 @@ describe "A Presenter" do
     it "with record and no template" do
       TestClassPresenter.new(TestClass.new, nil).should be_true
     end
+
+    context "and populate" do
+      before(:each) do
+        @test_class = TestClass.new
+        @template = Class.new
+        @test_presenter = TestClassPresenter.new(@test_class, @template)
+      end
+
+      subject {@test_presenter}
+
+      it "and populate the record field" do
+        subject.record.should equal @test_class
+      end
+
+      it "and populate the template field" do
+        subject.template.should equal @template
+      end
+
+      it "and populate the record_class field" do
+        subject.record_class.should equal @test_class.class
+      end
+    end
   end
 
   context "class should delgate" do
@@ -19,7 +41,8 @@ describe "A Presenter" do
   context "instance should" do
     before(:each) do
       @test_class = TestClass.new
-      @test_presenter = TestClassPresenter.new(@test_class, nil)
+      @template = Class.new
+      @test_presenter = TestClassPresenter.new(@test_class, @template)
       @test_presenter.record.should equal @test_class
     end
 
@@ -28,6 +51,11 @@ describe "A Presenter" do
     context "delegate" do
       it "method missing to record if method is missing" do
         @test_class.should_receive(:test_method).and_return("Success!")
+        subject.test_method.should match "Success!"
+      end
+
+      it "method missing to template if record lacks method" do
+        @template.should_receive(:test_method).and_return("Success!")
         subject.test_method.should match "Success!"
       end
 
